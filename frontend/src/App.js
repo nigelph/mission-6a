c7819430e10730d1981dac6edcc2c4e0931bf243
Nigel Phan <nigel.phan11@gmail.com>
2022-03-17 17:25:52 +1300

    general frontend setup with react-router-dom, react-boostrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from "./pages/Home/Home"
+import OrderHistory from './pages/OrderHistory/OrderHistory'
+import Footer from "./global-components/Footer/Footer"
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 function App() {
   return (
     <div className="App">
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/orders' element={<OrderHistory />}></Route>
+        </Routes>
+      </BrowserRouter>
+      <Footer />
     </div>
   );
 }
---
+++ frontend/src/global-components/Footer/BottomFooter/BottomFooter.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import style from './BottomFooter.module.css'
+function BottomFooter() {
+    return (
+        <footer className={style['test']}>hello</footer>
+    )
+}
+
+export default BottomFooter
