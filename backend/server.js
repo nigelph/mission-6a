b58df8d635999071574910236cbe7271eff2d1c9
Nigel Phan <nigel.phan11@gmail.com>
2022-03-17 16:07:04 +1300

    docker, react, node, mongo

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,87 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const cors = require('cors');
 const app = express();
 
-
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors())
+app.use(bodyParser.json());
+
+// Connect to LOCAL MongoDB URL
+// const url = 'mongodb://127.0.0.1:27017';
+
+// Mongo Atlas
+const url = 'mongodb+srv://nigel:pot3Toes@mission6.jnlpa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
+// Connect to mongo container in Docker
+// const url = 'mongodb://host.docker.internal:27017'
 
 
-app.listen(4000, () => console.log('Listening on port 4000'))
+// Connec to MongoDB
+MongoClient.connect(url, {
+
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, (err, client) => {
+    if (err) {
+        return console.log(err);
+    }
+
+    // Specify database you want to access
+    const db = client.db('mission6');
+    const products = db.collection('products')
+
+    // QuickSort function
+    const quickSort = list => {
+        if (list.length < 2) {
+            return list;
+        }
+
+        let pivot = list[0];
+        let left = [];
+        let right = [];
+        for (let i = 1; i < list.length; i++) {
+            // Compare alphabetical order of the first letter between two product names
+            if (list[i].product_name < pivot.product_name) {
+                left.push(list[i]);
+            } else {
+                right.push(list[i]);
+            }
+        }
+        return [...quickSort(left), pivot, ...quickSort(right)];
+    };
+
+
+    app.get('/products', (req, res) => {
+        products.find().toArray()
+            .then(results => {
+                console.log(quickSort(results))
+                res.send(quickSort(results))
+            })
+    })
+    app.post('/:input', (req, res) => {
+        const test = req.params.input;
+
+        res.send(test);
+    })
+
+    app.get('/hello', (req, res) => {
+        res.send('this is a change in the Hello api')
+    })
+
+    app.post('/:name/:desc/:country/:quantity/:cost', (req, res) => {
+        products.insertOne({ product_name: req.params.name, product_desc: req.params.desc, country_of_origin: req.params.country, quantity: req.params.quantity, cost: req.params.cost })
+        console.log(req.params.name)
+        console.log(req.params.desc)
+        console.log(req.params.country)
+        console.log(req.params.quantity)
+        console.log(req.params.cost)
+
+    })
+
+    console.log(`MongoDB Connected: ${url}`);
+});
+
+
+app.listen(8080, () => console.log('Listening on port 4000'))
---
+++ frontend/src/pages/Home/Home.js
@@ -0,0 +1,97 @@
+import React from 'react'
+import axios from 'axios'
+import { useState } from 'react'
+
+function Home() {
+
+    const [input, setInput] = useState()
+    const [post, setPost] = useState()
+    const [products, setProducts] = useState([])
+
+
+    const [name, setName] = useState("")
+    const [desc, setDesc] = useState("")
+    const [country, setCountry] = useState("")
+    const [quantity, setQuantity] = useState("")
+    const [cost, setCost] = useState("")
+    const [prod, setProd] = useState([name, desc, country, quantity, cost])
+
+
+    function handleClick() {
+        axios.post(`http://localhost:8080/${input}`)
+            .then((res) => {
+                setPost(res.data)
+            })
+    }
+
+    function handleClickGet() {
+        axios.get(`http://localhost:8080/products`)
+            .then((res) => {
+                console.log(res.data)
+                setProducts(res.data)
+            })
+    }
+
+    function postProduct(e) {
+        e.preventDefault()
+        axios.post(`http://localhost:8080/${name}/${desc}/${country}/${quantity}/${cost}`)
+            .then((res) => {
+                console.log(res.data)
+                setProd(res.data)
+            })
+    }
+
+    // function handle(e) {
+    //     const newData = { ...prod }
+    //     newData[e.target.id] = e.target.value
+    //     setProd(newData)
+    //     console.log(newData)
+    // }
+    return (
+        <div>
+            <input type='text' onChange={(e) => setInput(e.target.value)}></input>
+            <button onClick={handleClick}>Click me to post!</button>
+            <button onClick={handleClickGet}>Click me to get!</button>
+            <br />
+            <br />
+            <form onSubmit={(e) => postProduct(e)}>
+                <label>Product name</label>
+                <input type='text' onChange={(e) => setName(e.target.value)} />
+                <br />
+                <label>Product Description</label>
+                <input type='text' onChange={(e) => setDesc(e.target.value)} />
+                <br />
+                <label>Country of origin</label>
+                <input type='text' onChange={(e) => setCountry(e.target.value)} />
+                <br />
+                <label>Quantity</label>
+                <input type='text' onChange={(e) => setQuantity(e.target.value)} />
+                <br />
+                <label>Cost</label>
+                <input type='text' onChange={(e) => setCost(e.target.value)} />
+                <br />
+                <button >Click me to insert a new product</button>
+            </form>
+
+            <p>{post}</p>
+            <p>{prod}</p>
+
+            {
+                products.map(product =>
+                    <div style={{ textAlign: 'left' }}>
+                        <p><b>Product Name: </b>{product.product_name}</p>
+                        <p><b>Product Description:</b> {product.product_desc}</p>
+                        <p><b>Country of Origin: </b>{product.country_of_origin}</p>
+                        <p><b>Quantity: </b>{product.quantity}</p>
+                        <p><b>Cost: </b>{`$${product.cost}`}</p>
+                        <br />
+                    </div>
+                )
+            }
+
+        </div >
+    )
+}
+
+
+export default Home
